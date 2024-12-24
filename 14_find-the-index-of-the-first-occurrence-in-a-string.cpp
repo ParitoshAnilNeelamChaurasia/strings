db4e54a325cbae78304cffa112a6b6aa056f14bf
Paritosh Chaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-12-24 13:37:07 +0530

    Create 14_find-the-index-of-the-first-occurrence-in-a-string.cpp

---
+++ 14_find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,8 @@
+// https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/submissions/1486979430/
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        return haystack.find(needle) ;
+    }
+};
