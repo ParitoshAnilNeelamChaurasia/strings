e6fe8fb1f5fed65cf6dc6a421058eb7e6b7c8fd4
Paritosh Chaurasia <149140841+ParitoshAnilNeelamChaurasia@users.noreply.github.com>
2024-12-25 12:19:06 +0530

    Create 19_1_is subsequence.cpp

---
+++ 19_1_is subsequence.cpp
@@ -0,0 +1,22 @@
+// https://leetcode.com/problems/is-subsequence/description/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int m = s.size() ;
+        int n = t.size() ;
+
+        int i = 0 ; 
+        int j = 0 ;
+
+        while(i < m && j < n)
+        {
+            if(s[i] == t[j])
+            {
+                i++ ;
+            }
+            j++ ;
+        }
+        return i == m ;
+    }
+};
